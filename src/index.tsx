2d6222c24bfa221db762db589ea90a40423499dd
vinod898 <hvk898@gmail.com>
2023-12-22 23:36:18 +0530

    HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { HashRouter as Router } from 'react-router-dom';
 import  {AppRoutes}  from './Components/Routes'; // 0
 import { ThemeProvider, createTheme } from '@mui/material/styles'; // Import ThemeProvider and createTheme
 
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
   <ThemeProvider theme={theme}>
-      <Router>
+      <Router basename='/loan-amotization-calculator'>
         <AppRoutes />
       </Router>
     </ThemeProvider>
